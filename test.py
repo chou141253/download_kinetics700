ce1adda3e64f2c5954c846410b2656095cf8d726
chou <ntnuchou115423@gmail.com>
2020-03-26 10:09:08 +0800

    remove test file

--- test.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-cmd = "python3 {}".format("test2.py")
-out = subprocess.check_output(cmd,shell=True, stderr=subprocess.STDOUT)
-print(out.decode())
--- test2.py
+++
@@ -1,1 +0,0 @@
-print("this is program 2.")
